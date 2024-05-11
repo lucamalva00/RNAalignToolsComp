da96f7e8f882601c8cd400fdccc7afbf59d1cd65
lucamalva00 <luca.malvatani@studenti.unicam.it>
2024-05-11 12:51:16 +0200

    inserito lo script .py utilizza lo script .sh per scaricare il file pdb ed estrarre i dati interessati

---
+++ extractor_pdb.py
@@ -0,0 +1,61 @@
+import sys
+import subprocess
+import os
+import gzip
+import string
+
+def remove(string):
+    # Funzione per rimuovere gli spazi da una stringa
+    return string.replace(" ","")
+
+def run_bash_script(script_path, options):
+    # Funzione per eseguire uno script bash con le opzioni specificate
+    try:
+        subprocess.run([script_path] + options, check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Error running the bash script: {e}")
+        return False
+    return True
+
+# Ottieni gli ID PDB e della catena da riga di comando
+pdb_id = sys.argv[1]
+chain_id = sys.argv[2]
+
+# Salva l'ID PDB in un file
+with open("pdb_id.txt", "w") as file_with_id:
+    file_with_id.write(pdb_id)
+
+# Esegui lo script bash per scaricare il file PDB
+script_path = "./batch_download.sh"
+options = ["-f", "pdb_id.txt", "-p"]
+tar_file = pdb_id + ".pdb.gz"
+success = run_bash_script(script_path, options)
+if success:
+    # Decomprimi il file PDB scaricato
+    subprocess.run(["gzip" , "-d" , tar_file])
+    # Rimuovi il file temporaneo con l'ID PDB
+    os.remove("pdb_id.txt")
+    print("Bash script executed successfully.")
+else:
+    print("Error executing bash script.")
+
+# Apri il file PDB completo
+complete_pdb_file = open(pdb_id + ".pdb", "r")
+# Inserisci ogni riga del file come elemento della lista lines
+lines = complete_pdb_file.readlines()
+extracted_lines = []
+
+# Estrai le righe relative alla catena specificata
+for line in lines:
+    splitted_line = line.split()
+    for s in splitted_line:
+        remove(s)  # Rimuovi gli spazi da ogni elemento della riga
+    if splitted_line[0] == "ATOM" or splitted_line[0] == "HETATM": # Verifica che sia la parte interessata del file
+        if splitted_line[4] == chain_id:  # Verifica se la riga corrisponde alla catena specificata
+            extracted_line = "  ".join(splitted_line)  # Formatta la riga estratta
+            extracted_lines.append(extracted_line)  # Aggiungi la riga estratta all'elenco
+
+# Scrivi le righe estratte in un nuovo file PDB
+with open("extracted_chain_pdb.pdb", "w") as extracted_chain:
+    for line in extracted_lines:
+        extracted_chain.write(line + "\n")
